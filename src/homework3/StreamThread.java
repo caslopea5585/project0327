4d540445bd2669b9c120a1a9fa2e1918da24a3fb
user\user1 <caslopea5585@gmail.com>
2017-03-29 17:51:19 +0900

    0329 수업종료

--- src/homework3/StreamThread.java
+++ src/homework3/StreamThread.java
@@ -1,14 +1,13 @@
 package homework3;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 import javax.swing.JLabel;
@@ -29,6 +28,8 @@ public class StreamThread extends Thread {
 	long ori_size,dest_size;
 	float percent=0;
 	JLabel label;
+	File f;
+	
 	
 	public void run() {
 		while(true){
@@ -39,10 +40,12 @@ public class StreamThread extends Thread {
 				
 				writer2.write(data);
 				
+				
 				System.out.println("data Å©±â?"+data.length());
+				
 				count +=data.length();
 				System.out.println("count"+count+"dest"+dest_size);
-				
+				bar.setValue(count);
 				
 				/*//fos.write(data);
 				count++;
